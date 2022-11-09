6230514f860fa7b42e462c07f734cc06135b255f
brendanle <brendan.mi.le@outlook.com>
2022-11-09 14:49:05 -0700

    fixed bugs

--- js/criteria/render/renderTag.js
+++ js/criteria/render/renderTag.js
@@ -3,15 +3,17 @@ function renderTag({ name, color, logics }) {
   const tag = document.createElement('li');
   tag.classList.add('tag__item');
   tag.innerHTML = `
-      <div class="tag__color">
-        <input class="color__picker" type="color" id="favcolor" name="favcolor" value="${color}" />
+      <div class = "tag__label">
+        <div class="tag__color">
+          <input class="color__picker" type="color" id="favcolor" name="favcolor" value="${color}" />
+        </div>
+        <input class="tag__text" type="text" value="${name}" name="tag-1" />
+        <span class="tag__divider">:</span>
       </div>
-      <input class="tag__text" type="text" value="${name}" name="tag-1" />
-      <span class="tag__divider">:</span>
       <ul class = "tags__chain">
         ${renderTagOptions(logics)}
+        <span class="material-symbols-outlined tag__add"> add_circle </span>
       </ul>
-      <span class="material-symbols-outlined tag__add"> add_circle </span>
     `;
 
   filters.appendChild(tag);
