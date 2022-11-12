072a01f5a7665962880e85902842a670a5f5fd11
Grable <gabrieljarrard14@gmail.com>
2022-11-11 22:24:24 -0700

    bugged map

--- js/data.js
+++ js/data.js
@@ -1,13 +1,16 @@
 (async () => {
   const { data: sites } = await d3.json('data/Sites.json');
+  const { data: geoJSON } = await d3.json('data/geoJSON.json');
 
+
+  console.log(geoJSON)
   const categories = new Set(sites.map(({ categories }) => categories).flat());
   const awards = new Set(sites.map(({ awards }) => awards).flat());
   const countries = new Set(sites.map(({ countries }) => countries).flat());
   awards.delete(undefined);
   console.log(categories);
   console.log(awards);
-  const worldMap = new MapVis(countries);
+  const worldMap = new MapVis(sites, countries,categories, geoJSON);
 })();
 //todo - go through each category and filter by awards
 
