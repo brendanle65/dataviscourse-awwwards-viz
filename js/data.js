bc6016bd3716e8cf6cc7dc89797187c93013bd54
brendanle <brendan.mi.le@outlook.com>
2022-11-11 23:26:02 -0700

    merged changes

--- js/data.js
+++ js/data.js
@@ -37,14 +37,10 @@ const globalState = {
 
 (async () => {
   const { data: sites } = await d3.json('data/Sites.json');
-  const { data: geoJSON } = await d3.json('data/geoJSON.json');
+  const geoJSON = await d3.json('data/geoJSON.json');
 
   console.log(geoJSON);
   const categories = new Set(sites.map(({ categories }) => categories).flat());
-  const awards = new Set(sites.map(({ awards }) => awards).flat());
   const countries = new Set(sites.map(({ countries }) => countries).flat());
-  awards.delete(undefined);
-  console.log(categories);
-  console.log(awards);
   const worldMap = new MapVis(sites, countries, categories, geoJSON);
 })();
